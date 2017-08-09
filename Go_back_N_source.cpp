44ceb458fa10e10468d6f5d42e9c966966ce22cd
Abdullah Al Masud Tushar <rctushar07@gmail.com>
2017-08-10 02:26:10 +0600

    Update Go_back_N_source.cpp

--- Go_back_N_source.cpp
+++ Go_back_N_source.cpp
@@ -1,9 +1,11 @@
 /**
    ~~~ BISMILLAH HIR RAHMANIR RAHIM ~~~
+
    Problem Name: Go-Back-N Sliding Window Protocol with CRC32 error detection
        Platform: Data Communication
       Algorithm: Go-Back-N , CRC32
   Author's Name: RC Tushar
+
 ----------------------------------------------------
 let the light of the Shelley moon die tonight
 let the sky cover herself with dark shroud, tonight
@@ -11,7 +13,9 @@ but not the hope that promised to live
 Just say her, dear, don't sleep tonight
 die another day, die another night
 ----------------------------------------------------
+
 "Don't follow others,instead learn from their experience and make your own path"
+
 **/
 
 
@@ -27,85 +31,86 @@ die another day, die another night
 using namespace std;
 
 
-string frame[50];
+char frame[500][500];
 int n,w;
 
-char Reminder[500]= {},Recieved[500]= {};
+char Reminder[500]= {};
 
-void crc(string input,string P)  // CRC error detection function
+char* crc(char *input,char key[])
 {
     int i,j,keylen,msglen;
+    char temp[500],quot[100],rem[500],key1[500];
+    char yy[500]= {};
 
-    string key1(500,' ');
-    char temp[500],rem[500];
+    keylen=strlen(key);
+    msglen=strlen(input);
 
-    keylen=P.size();
-    msglen=input.size();
-    key1=P;
-
+    strcpy(key1,key);
     for (i=0; i<keylen-1; i++)
         input[msglen+i]='0';
 
     for (i=0; i<keylen; i++)
         temp[i]=input[i];
 
-
     for (i=0; i<msglen; i++)
     {
-        if(temp[0]=='0')
+        quot[i]=temp[0];
+        if(quot[i]=='0')
         {
             for (j=0; j<keylen; j++)
-                P[j]='0';
+                key[j]='0';
         }
         else
         {
             for (j=0; j<keylen; j++)
-                P[j]=key1[j];
+                key[j]=key1[j];
         }
 
         for (j=keylen-1; j>0; j--)
         {
-            if(temp[j]==P[j])
+            if(temp[j]==key[j])
                 rem[j-1]='0';
             else
                 rem[j-1]='1';
         }
+
         rem[keylen-1]=input[i+keylen];
         strcpy(temp,rem);
     }
 
     strcpy(rem,temp);
-
     for (i=0; i<keylen-1; i++)
         Reminder[i]=rem[i];
 
     for (i=0; i<msglen; i++)
-        Recieved[i]=input[i];
+        yy[i]=input[i];
     for (j=0; i<msglen+keylen-1; i++,j++)
-        Recieved[i]=rem[j];
+        yy[i]=rem[j];
+
+    return yy;
 }
 
 
-bool ck_err(string input,string P)  // This function takes a frame and returns the state (success/damage) of the received frame
+bool ck_err(char* input,char P[])  // This function takes a frame and returns the state (success/damage) of the received frame
 {
     int rr,gg;
-    crc(input,P);     // CRC32 to find the FCS and Resultant Message
+    char zz[1000];
+    strcpy(zz,crc(input,P));     // CRC32 to find the FCS and Resultant Message
 
     rr=rand()%4;    // randomly picks a frame to manipulate it.
-                    // If rr=0 it will manipulate the current frame
+    // If rr=0 it will manipulate the current frame
 
     gg=rand()%10;   // randomly picks a position from 0-9 in the
-                    // current frame to flip the gg position
+    // current frame to flip the gg position
 
     if(!rr)
     {
-        if(Recieved[gg]=='0')
-            Recieved[gg]='1';
+        if(zz[gg]=='0')
+            zz[gg]='1';
         else
-            Recieved[gg]='0';
+            zz[gg]='0';
     }
-
-    crc(Recieved,P); // CRC32 to check the received message's reminder
+    strcpy(zz,crc(zz,P)); // CRC32 to check the received message's reminder
 
     pf("\n\n Reminder Found:  %s\n",Reminder);
     for(int i=0; Reminder[i]; i++)
@@ -114,12 +119,12 @@ bool ck_err(string input,string P)  // This function takes a frame and returns t
     return true; // if reminder is zero returns true
 }
 
-string genar()   // generates and returns random data for a frame
+char* genar()   // generates and returns random data for a frame
 {
-    string Reminder;
+    char xx[500];
     for(int i=0; i<51; i++)
-        Reminder.pb((char)((rand()%2)+48));
-    return Reminder;
+        xx[i]=(char)((rand()%2)+48);
+    return xx;
 }
 
 void show()     // prints bar in middle of the frames
@@ -174,7 +179,7 @@ void Print_discard(int x,int y) // marks & prints discarded frames
     cout<<endl<<endl;
 }
 
-int chunk(int x,int y,string P)  // Shows all states of the Go-Back N protocol
+int chunk(int x,int y)  // Shows all states of the Go-Back N protocol
 {
     system("cls");
     pf("\n\n## Frame %d to %d is Sending\n\n",x,y);
@@ -184,9 +189,11 @@ int chunk(int x,int y,string P)  // Shows all states of the Go-Back N protocol
 
     for(int i=x; i<=y; i++)  // Sending frames (x-y)
     {
+        char P[]="100000100110000010001110110110111";  // P value of CRC32
+
         system("cls");
-        pf("\n\nData Of Frame %d: %s\n",i,frame[i].c_str());
-        pf("\n\nData Of Divisor: %s ",P.c_str());
+        pf("\n\nData Of Frame %d: %s\n",i,frame[i]);
+        pf("\n\nData Of Divisor: %s ",P);
 
         if(ck_err(frame[i],P))
         {
@@ -217,7 +224,6 @@ int chunk(int x,int y,string P)  // Shows all states of the Go-Back N protocol
 
 int main()
 {
-    string P="100000100110000010001110110110111";  // P value of CRC32
     srand(time(NULL));
 
     pf("Give Total Frame Size: ");
@@ -227,11 +233,11 @@ int main()
 
     //10 random 50 bit-frame generated
     for(int i=0; i<n; i++)
-        frame[i]=genar();
+        strcpy(frame[i],genar());
 
     int i=0;
     while(i<n)
-        i=chunk(i,min(i+w-1,n-1),P);
+        i=chunk(i,min(i+w-1,n-1));
 
     return 0;
 }
